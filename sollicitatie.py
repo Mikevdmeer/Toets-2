a0e5f72ae14eaabb4aa9bb59181a441164b41b8c
Mikevdmeer <99081100@mydavinci.nl>
2024-10-18 10:51:38 +0200

    aangepast voor opdracht

--- sollicitatie.py
+++ sollicitatie.py
@@ -8,8 +8,15 @@ ervaring_dieren_dressuur_ok = 4
 ervaring_jongleren_ok = 5
 ervaring_acrobatiek = 3
 Vrachtwagen_rijbewijs = input("Heeft u een Vrachtwagen rijbewijs? (ja/nee): ")
+if Vrachtwagen_rijbewijs == "banaan":
+    raise NameError('een banaan is geen rijbewijs!')
 hoge_hoed = input("Bent u in bezit van een hoge hoed? (ja/nee): ")
+if hoge_hoed == "lage hoed":
+    raise TypeError('dat is niet de bedoeling')
 gewicht = input("Bent u zwaarder dan 90 kg en lichter dan 120 kg? (ja/nee): ")
+if gewicht == "10":
+    raise ValueError('dat is niet gezond')
+    exit()
 ervaring_dieren_dressuur = int(input("Hoeveel jaar ervaring heeft u met dieren-dressuur (voer het als een getal in): "))
 ervaring_jongleren = int(input("Hoeveel jaar ervaring heeft u met jongleren (voer het als een getal in): "))
 ervaring_acrobatiek = int(input("Hoeveel jaar ervaring heeft u met acrobatiek? (voer het als een getal in): "))
